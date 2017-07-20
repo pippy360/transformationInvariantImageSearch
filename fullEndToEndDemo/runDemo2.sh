45c2c485fd12fd0d7c943d6064f9a4e20ce8ad21
Tom Murphy <pippy360@users.noreply.github.com>
2017-07-20 12:35:36 +0100

    Update runDemo2.sh

--- fullEndToEndDemo/runDemo2.sh
+++ fullEndToEndDemo/runDemo2.sh
@@ -1,2 +1,5 @@
-./runDemo insert ./inputImages/mona.jpg ./inputImages/monaComposite.jpg ./inputImages/van_gogh.jpg
+#add the two orginal images
+./runDemo insert ./inputImages/mona.jpg ./inputImages/van_gogh.jpg
+
+#query the database using the composite of the two images
 ./runDemo lookup ./inputImages/monaComposite.jpg
