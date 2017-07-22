e04f20c14cc0b9edab6be17b85d19165b2731cdc
Tom Murphy <pippy360@users.noreply.github.com>
2017-07-22 09:06:56 +0100

    Update commandsToRun.sh

--- fullEndToEndDemo/commandsToRun.sh
+++ fullEndToEndDemo/commandsToRun.sh
@@ -1,7 +1,7 @@
 #Oh boy, this is a big install, grab a cup of tea and enjoy the wait...
 sudo apt-get install git cmake g++ redis-server libboost-all-dev libopencv-dev python-opencv python-numpy python-scipy -y
 
-#remove annoying opencv error
+#remove annoying opencv error: https://stackoverflow.com/questions/12689304/ctypes-error-libdc1394-error-failed-to-initialize-libdc1394
 sudo ln /dev/null /dev/raw1394
 
 #make it
