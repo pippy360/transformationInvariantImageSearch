769cbe354f7edfd0399cf81d133503a848365466
Tom Murphy <pippy360@users.noreply.github.com>
2017-07-24 11:24:39 +0100

    Update setup.sh

--- fullEndToEndDemo/setup.sh
+++ fullEndToEndDemo/setup.sh
@@ -1,4 +1,5 @@
 #Oh boy, this is a big install, grab a cup of tea and enjoy the wait...
+sudo apt-get update
 sudo apt-get install git cmake g++ redis-server libboost-all-dev libopencv-dev python-opencv python-numpy python-scipy -y
 
 #remove annoying opencv error: https://stackoverflow.com/questions/12689304/ctypes-error-libdc1394-error-failed-to-initialize-libdc1394
