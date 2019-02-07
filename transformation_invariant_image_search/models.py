db47c7dfcb51eeeae58d9fc735b761a1b11e72b9
rachmadaniHaryono <foreturiga@gmail.com>
2019-02-07 19:19:16 +0800

    chg: dev: remove unused table

--- transformation_invariant_image_search/models.py
+++ transformation_invariant_image_search/models.py
@@ -20,14 +20,6 @@ DATA_DIR = user_data_dir('transformation_invariant_image_search', 'Tom Murphy')
 pathlib.Path(DATA_DIR).mkdir(parents=True, exist_ok=True)
 DEFAULT_IMAGE_DIR = os.path.join(DATA_DIR, 'image')
 
-triangle_points = DB.Table(
-    'triangle_points',
-    DB.Column('triangle_phash_id', DB.Integer, DB.ForeignKey('triangle_phash.id'), primary_key=True),
-    DB.Column('point_id', DB.Integer, DB.ForeignKey('point.id'), primary_key=True))
-triangle_phashes = DB.Table(
-    'triangle_phashes',
-    DB.Column('triangle_phash_id', DB.Integer, DB.ForeignKey('triangle_phash.id'), primary_key=True),
-    DB.Column('phash_id', DB.Integer, DB.ForeignKey('phash.id'), primary_key=True))
 checksum_phashes = DB.Table(
     'checksum_phashes',
     DB.Column('checksum_id', DB.Integer, DB.ForeignKey('checksum.id'), primary_key=True),
@@ -56,38 +48,12 @@ class Checksum(Base):
         return {k: getattr(self, k) for k in keys}
 
 
-class Point(Base):
-    x = DB.Column(DB.Integer(), nullable=False)
-    y = DB.Column(DB.Integer(), nullable=False)
-
-    def __repr__(self):
-        templ = '<Point(x,y={0.x},{0.y}})>'
-        return templ.format(self)
-
-
 class Phash(Base):
     value = DB.Column(DB.String(), unique=True, nullable=False)
 
     def __repr__(self):
         templ = '<Phash(v={0.value})>'
         return templ.format(self)
-
-
-class TrianglePhash(Base):
-    checksum_id = DB.Column(DB.Integer, DB.ForeignKey('checksum.id'), nullable=False)
-    checksum = DB.relationship('Checksum', backref='triangle_phashes', lazy=True)
-    points = DB.relationship('Point', secondary=triangle_points, lazy='subquery',
-        backref=DB.backref('triangle_phashes', lazy=True))
-    phashes =  DB.relationship('Phash', secondary=triangle_phashes, lazy='subquery',
-        backref=DB.backref('triangle_phashes', lazy=True))
-
-    def __repr__(self):
-        templ = '<TrianglePhash(checksum={0.checksum.value[:7]}, points=[{1}], phashes=[{2}])>'
-        return templ.format(
-            self,
-            ','.join(['({0.x, 0.y})'.format(x) for x in Point]),
-            ','.join(['{0.value}'.format(x) for x in Point]),
-        )
 
 
 def get_or_create(session, model, **kwargs):
